2bc98610fcaef9545334278a2edcab292b5a7c02
Steven <stntsai93@gmail.com>
2021-12-07 18:19:41 -0500

    infura address

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -9,7 +9,7 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    "https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c"
+    "https://rinkeby.infura.io/v3/cf0d54c2cc2e4978852c2c5e448bb19f"
   );
   web3 = new Web3(provider);
 }
