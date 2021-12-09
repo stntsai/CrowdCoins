45a1f5b9681918e475cb8549bd6589ffe7d1d4df
Steven <stntsai93@gmail.com>
2021-12-09 14:25:34 -0500

    add semantic ui css

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,30 @@
 import React, {Component} from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { Card } from 'semantic-ui-react';
+
 import factory from '../ethereum/factory';
 
 
 class CampaignIndex extends Component {
     static async getInitialProps(){
         const campaigns = await factory.methods.getDeployedCampaigns().call();
-        console.log(campaigns)
         return { campaigns };
     }
 
+    renderCampaigns(){
+        const items = this.props.campaigns.map(address=>{
+            return {
+                header: address,
+                description: <a>View description</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items}/>;
+    }
+
     render(){
-        return <div>{this.props.campaigns[0]}</div>;
+        return <div>{this.renderCampaigns()}</div>;
     }
 }
 
