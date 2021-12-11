58f961880dfa4d8b6c50792c8f75048dac76c834
Steven <stntsai93@gmail.com>
2021-12-11 14:41:44 -0500

    requests button and page

---
+++ pages/campaigns/requests/index.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class RequestIndex extends Component{
+    render(){
+        return (
+            <h1>request</h1>
+        )
+    }
+}
+
+export default RequestIndex;
