b8507262a3392d32276357c77541222b2c6a0383
Steven <stntsai93@gmail.com>
2021-12-11 12:01:57 -0500

    redeploy and update factory address

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const contract = new web3.eth.Contract(
     JSON.parse(CampaignFactory.interface),
-    '0xb24FBB15245Cd3C9D914B5199feAc29D88Ca6623' //deployed address
+    '0x56Ff3991400D4649bC06c4496Fd13834fd05f64b' //deployed address
 );
 
 export default contract;
