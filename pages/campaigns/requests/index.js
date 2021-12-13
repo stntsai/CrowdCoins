afc2055f849e252c3804105796a6eb53b459f555
Steven <stntsai93@gmail.com>
2021-12-13 12:28:12 -0500

    fetch every requests from contract

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,13 +2,23 @@ import React,{Component} from "react";
 import Layout from "../../../components/Layout";
 import { Button } from 'semantic-ui-react';
 import { Link } from '../../../routes';
+import Campaign from '../../../ethereum/campaign';
 
 class RequestIndex extends Component{
 
     static async getInitialProps(props){
         const { address } = await props.query;
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
 
-        return { address };
+        // use requestCount as the total number we need to do the interation on
+        // methods.requests function to fetch the request object stored in the contract
+        const requests = await Promise.all(
+            Array(requestCount).fill().map((element, index) => {
+                return campaign.methods.requests(index).call();
+            })
+        )
+        return { address, requests };
     }
 
     render(){
