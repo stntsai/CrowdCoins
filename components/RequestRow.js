2629b33fab1dc07d6c4f469e3f73ef44d4101f02
Steven <stntsai93@gmail.com>
2021-12-14 10:54:15 -0500

    vote & finalize interaction

--- components/RequestRow.js
+++ components/RequestRow.js
@@ -6,26 +6,46 @@ import Campaign from '../ethereum/campaign';
 class RequestRow extends Component {
     onApprove = async () => {
         const campaign = await Campaign(this.props.address);
-        const accounts = await web3.eth.getAccounts()
+        const accounts = await web3.eth.getAccounts();
         await campaign.methods.approveRequest(this.props.id).send({
             from: accounts[0]
         });
 
     }
+
+    onFinalize = async() =>{
+        const campaign = Campaign(this.props.address);
+        const accounts = await web3.eth.getAccounts();
+
+        await campaign.methods.finalizeRequest(this.props.id).send({
+            from: accounts[0]
+        });
+    }
     render(){
         const { Row, Cell } = Table;
         const {id, request, shareholdersCount} = this.props;
+        const readyToFinalize = request.votes > shareholdersCount/2;
+
         return(
-            <Row>
+            <Row disabled={request.complete} positive={readyToFinalize && !request.complete}>
                 <Cell>{id}</Cell>
                 <Cell>{request.description}</Cell>
                 <Cell>{web3.utils.fromWei(request.value,'ether')}</Cell>
                 <Cell>{request.recipient}</Cell>
                 <Cell>{request.votes}/{shareholdersCount}</Cell>
                 <Cell>
-                    <Button color="green" basic onClick={this.onApprove}>
-                        Approve
-                    </Button>
+                    {request.complete? null: (
+                        <Button color="green" basic onClick={this.onApprove}>
+                            Approve
+                        </Button>
+                    )}
+                </Cell>
+                <Cell>
+                    {request.complete? null: (
+                        <Button color="teal" basic onClick={this.onFinalize}>
+                            Finalize
+                        </Button>
+                    )}
                 </Cell>
             </Row>
 
